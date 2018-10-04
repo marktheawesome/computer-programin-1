f916a65908019aee2a65b162f750fb81003ef8c4
marktheawesome <markgyomory@outlook.com>
2018-10-03 21:52:03 -0400

    Final

--- guess-a-number-ai/guess_a_number_ai.py
+++ guess-a-number-ai/guess_a_number_ai.py
@@ -10,11 +10,13 @@ import math
 # config
 low = 1
 high = 1000
-
+tries_taken = 1
+spread = high - low
+limit = math.ceil(math.log(spread,2))
 
 # helper functions
 def show_start_screen():
-    print('''
+    print (R'''
 
                                                            _                  ___  _____ 
                                                           | |                / _ \|_   _|
@@ -36,7 +38,7 @@ def player_name():
     return name 
 
 def show_credits():
-    pass
+    print("This Game was made by Mark.")
     
 def get_guess(current_low, current_high):
     """
@@ -46,16 +48,16 @@ def get_guess(current_low, current_high):
     average = current_added//2
     return average
 
-def pick_number(name):
+def pick_number(name,limit):
     """
     Ask the player to think of a number between low and high.
     Then  wait until the player presses enter.
     """
-    print (name + " pick a number between 1 and 1000")
+    print (name + " pick a number between 1 and 1000. I'm willing to bet I'll get it in " + str(limit) + " or less.")
     input ()
 
 
-def check_guess(guess):
+def check_guess(guess,name,tries_taken):
     """
     Computer will ask if guess was too high, low, or correct.
 
@@ -63,7 +65,8 @@ def check_guess(guess):
              0 if the guess was correct
              1 if the guess was too high
     """
-    print("I guess " +str(guess) + ("."))
+
+    print(str(name) +" I guess " + str(guess) + (". ") + ("This is try number ") + str(tries_taken))
     print("Was I too high or too low or correct? (h/l/c)")
     put = input()
     if put == "h":
@@ -95,24 +98,29 @@ def play_again():
         else:
             print("I don't understand. Please enter 'y' or 'n'.")
 
-def play(name):
+def play(name,tries_taken,limit):
     
     current_low = low
     current_high = high
-    check = -1
-    
-    pick_number(name)
+    check = -1 
+
+    pick_number(name,limit)
     
     while check != 0:
         guess = get_guess(current_low, current_high)
-        check = check_guess(guess)
-
-        if check == -1:
+        check = check_guess(guess,name, tries_taken)
+        tries_taken = tries_taken + 1 
+        if limit == tries_taken:
+            print("You must have done something wrong. I've gessed the most logical wa possible. It impossible that I've lost.")
+            return
+        elif check == -1:
             # adjust current_low
             current_low = guess + 1
         elif check == 1:
             # adjust current_high
             current_high = guess - 1 
+        if limit == tries_taken:
+            print("You must have done something wrong. I've gessed the most logical wa possible. It impossible i lost.")
 
     show_result(name)
 
@@ -124,7 +132,7 @@ name = player_name ()
 playing = True
 
 while playing:
-    play(name)
+    play(name,tries_taken,limit)
     playing = play_again()
 
 show_credits()
